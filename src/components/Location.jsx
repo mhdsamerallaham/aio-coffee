ab21299b2a877182083d4bf3e4ef4894a9e83a37
mhdsamerallaham <samerallaham3@gmail.com>
2026-01-20 10:47:59 -0800

    Update location to Istanbul - Change Google Maps to Şişli address and Turkish contact info

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -8,7 +8,7 @@ const Location = () => {
         
         <div className="map-container">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.2219901290355!2d-74.00369368400567!3d40.71312937933039!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a316bb7b3b5%3A0x4e5c7e5c7e5c7e5c!2sEmpire%20State%20Building!5e0!3m2!1sen!2sus!4v1234567890"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3006.735746234567!2d28.97716391531738!3d41.05378797929462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cab765a5c5c5c5c%3A0x123456789abcdef!2sCumhuriyet%2C%20Rumeli%20Cd.%2094%2FA%2C%2034380%20%C5%9Ei%C5%9Fli%2F%C4%B0stanbul!5e0!3m2!1str!2str!4v1234567890"
             className="map-iframe"
             allowFullScreen=""
             loading="lazy"
@@ -19,13 +19,13 @@ const Location = () => {
         
         <div className="location-info">
           <p className="location-address">
-            123 Coffee Street<br />
-            New York, NY 10001<br />
-            United States
+            Cumhuriyet, Rumeli Cd. 94/A<br />
+            34380 Şişli/İstanbul<br />
+            Türkiye
           </p>
           <p className="location-contact">
-            Phone: (555) 123-4567<br />
-            Email: hello@aiocoffee.com
+            Telefon: +90 212 123 45 67<br />
+            Email: info@aiocoffee.com
           </p>
         </div>
       </div>
